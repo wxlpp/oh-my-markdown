af5d616d79c7f2b1bdc93188659140f06bb105b3
Evan <coder.wxlpp@gmail.com>
2025-11-27 02:55:48 +0800

    Update .gitignore for macOS/iOS development

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MarkdownKit",
+    platforms: [.iOS(.v18), .macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +13,14 @@ let package = Package(
             targets: ["MarkdownKit"]
         ),
     ],
+    dependencies: [.package(url: "https://github.com/swiftlang/swift-markdown.git", branch: "main")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MarkdownKit"),
+            name: "MarkdownKit",
+            dependencies: [.product(name: "Markdown", package: "swift-markdown")]
+        ),
         .testTarget(
             name: "MarkdownKitTests",
             dependencies: ["MarkdownKit"]
